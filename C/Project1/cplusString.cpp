7ca443dca36a21900ac95245c3546225c6df94c7
Sooooyeon <tn7278@naver.com>
2024-03-20 13:22:03 +0900

    객체의 동적 메모리 할당

---
+++ C/Project1/cplusString.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class String 
+{
+public:
+	String(char ch, int nSize) {
+		nLength = nSize;
+		pBuffer = new char[nLength + 1];
+		memset(pBuffer, ch, nLength);
+		// memset 함수는 메모리의 내용(값)을 원하는 크기만큼 특정 값으로 세팅
+		// memset함수는 메모리 상의 일정 블록을 통채로 원하는 값으로 설정해 주는 함수
+		// void *memset(void *s, int c, size_t n);
+		// memset 함수는 메모리상에서 포인터 s가 가리키는 위치부터 총 n bytes를 c로 채워준다.
+		// memset은 포인터 s가 가리키는 주소를 가리키는 포인터를 반환한다.
+		
+		pBuffer[nLength] = '\0';
+		cout << "pBuffer : " << pBuffer << endl;
+	}
+	~String() {
+		delete[] pBuffer;
+	}
+
+	// 대입연산자 오버로딩 
+	void operator = (const String& s) 
+	{
+		delete this->pBuffer;
+		this->nLength = s.nLength;
+		this->pBuffer = new char[this->nLength + 1];
+		strcpy_s(this->pBuffer, this->nLength +1, s.pBuffer);
+	}
+
+	/*void operator + (const String& s) 
+	{
+
+	}*/
+
+	void SetData() {
+		cout << "pBuffer : " << this->pBuffer << endl;
+	}
+
+private:
+	char* pBuffer;
+	int nLength;
+
+};
+
+void main() 
+{
+	String str1('A', 3);
+	String str2('B', 5);
+
+	str2 = str1; // str2 의 pBuffer가 'A'의 주소를 가르키게됨 소멸할때 문제가 됨
+	// str2.operator=(str1); // 윗줄의 코드와 같은 표현
+
+	str2.SetData();
+
+}
--- C/Project1/cplus_클래스.cpp
+++ C/Project1/cplus_클래스.cpp
@@ -8,49 +8,63 @@ using namespace std;
 // 함수의 구현부를 class안에 정의할 수 있지만,
 // class 는 header 파일로 분리하는게 전통적이라 구현을 따로한다.
 // class 안에 함수의 구현부를 작성해도 무관
+
 class MousePoint {
+
 private:
 	int x;
 	int y;
 public:
-	void SetXY(int nX, int nY);
-};
-// 함수의 접근제어자가 private인경우도 구현은 클래스 외부에서 가능.
-// 클래스 내부의 접근제어자는 생략 불가능. 
-
-void MousePoint::SetXY(int nX, int nY) {
-	x = nX;
-	y = nY;
-}
-
-class Test 
-{
-private: // 멤버변수를 public으로 사용하는것은 객체지향 개념에 위배됨
-	int x;
-	int y;
-
-public:
-	Test(int nX, int nY) 
+	MousePoint(int nX, int nY)
 	{
 		x = nX;
 		y = nY;
 		cout << "전달인자가 2개인 생성자" << endl;
 	}
-	Test(){
+	MousePoint() {
 		cout << "디폴트 생성자" << endl;
 	}
-	~Test() {
+	~MousePoint() {
 		cout << "소멸자" << endl;
 	}
-	int GetX(){ return x; };
-	int GetY(){ return y; };
-	void SetXY(int nX, int nY);
+	int GetX() { return x; };
+	int GetY() { return y; };
+	void SetXY(int nX, int nY) {
+		x = nX;
+		y = nY;
+	}
 };
+// 함수의 접근제어자가 private인경우도 구현은 클래스 외부에서 가능.
+// 클래스 내부의 접근제어자는 생략 불가능. 
 
-void Test::SetXY(int nX, int nY) {
-	x = nX;
-	y = nY;
-}
+//class Test 
+//{
+//private: // 멤버변수를 public으로 사용하는것은 객체지향 개념에 위배됨
+//	int x;
+//	int y;
+//
+//public:
+//	Test(int nX, int nY) 
+//	{
+//		x = nX;
+//		y = nY;
+//		cout << "전달인자가 2개인 생성자" << endl;
+//	}
+//	Test(){
+//		cout << "디폴트 생성자" << endl;
+//	}
+//	~Test() {
+//		cout << "소멸자" << endl;
+//	}
+//	int GetX(){ return x; };
+//	int GetY(){ return y; };
+//	void SetXY(int nX, int nY);
+//};
+//
+//void Test::SetXY(int nX, int nY) {
+//	x = nX;
+//	y = nY;
+//}
 
 void main() {
 	// MousePoint pt; // 클래스로 선언한 변수는 객체
@@ -63,45 +77,59 @@ void main() {
 	//pt.x = 0; // 기본값이 private으로 데이터에 접근 불가능
 	//pt.SetXY(10, 20);
 
-	Test ts2;
+	// Test ts2;
 
 	// 스택 메모리에 정적 객체 생성
-	Test ts(12, 20); // 생성자 함수를 통한 객체 생성 및 초기화
-	cout << ts.GetX() << endl;
-	cout << ts.GetY() << endl;
+	// Test ts(12, 20); // 생성자 함수를 통한 객체 생성 및 초기화
+	// cout << ts.GetX() << endl;
+	// cout << ts.GetY() << endl;
 
 	// 힙 메모리에 동적으로 객체 생석
-	Test* ts3 = new Test(11, 22);
-	cout << ts3->GetX() << endl;
-	cout << ts3->GetY() << endl;
+	// Test* ts3 = new Test(11, 22);
+	// cout << ts3->GetX() << endl;
+	// cout << ts3->GetY() << endl;
 
 
-	delete ts3; // 사용 후 삭제
+	// delete ts3; // 사용 후 삭제
 
-	ts.SetXY(50,100);
-	cout << ts.GetX() << endl;
-	cout << ts.GetY() << endl;
+	// ts.SetXY(50,100);
+	// cout << ts.GetX() << endl;
+	// cout << ts.GetY() << endl;
 
 
 	// 소멸자 - 객체가 소멸할 때 자동 호출됨. 
 	// // 2개의 객체를 생성했기때문에 소멸자가 2번 호출됨오버로딩 안됨
 
 
-	Test pt[3] = { Test(10,20), Test(100,200), Test(500,600) }; // 초기화
+	// Test pt[3] = { Test(10,20), Test(100,200), Test(500,600) }; // 초기화
 
 	// pt[0].SetXY(10, 20);
 	// pt[1].SetXY(100, 200);
-	for (int i = 0; i < 3; i++) {
+	/*for (int i = 0; i < 3; i++) {
 		cout << pt[i].GetX() << endl;
 		cout << pt[i].GetY() << endl;
 
-	}
+	}*/
 	// cout << pt[0].GetX() << endl;
 	// cout << pt[1].GetX() << endl;
 
 
+	MousePoint* pArr[3];
 
+	pArr[0] = new MousePoint(10, 20);
+	pArr[1] = new MousePoint(100, 200);
+	pArr[2] = new MousePoint(1000, 2000);
 
+	for (int i = 0; i < 3; i++) {
+		cout << pArr[i]->GetX() << endl;
+		cout << pArr[i]->GetY() << endl;
+	}
 
+	for (int i = 0; i < 3; i++) {
+		delete pArr[i];
+		// 힙 메모리에서 배열을 만들었을 경우에만 delete[ ]
+		// 스택에서 만들었을 경우 하나씩 없애줘야함 
+		// 스택에서 힙 메모리 주소를 없애면 해당 힙 영역의 데이터가 제거됨
+	}
 
 }	
