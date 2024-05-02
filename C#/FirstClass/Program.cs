2f70dfb12546b3aa31c531374757b68ab04f0da2
Sooooyeon <tn7278@naver.com>
2024-05-02 17:45:55 +0900

    C# DAY3 오버로딩, 오버라이딩

---
+++ C#/FirstClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    public class Dog
+    {
+        protected int eyes, nose, mouse, ears;
+
+        public Dog() 
+        {
+            eyes = 2;
+            nose = 1;
+            mouse = 1;
+            ears = 2;
+        }
+
+        virtual public void Bark() {
+            Console.WriteLine("멍멍");
+        }
+    }
+
+    public class Pudle : Dog
+    {
+        public override void Bark()
+        {
+            Console.WriteLine("월월");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+
+            Pudle pudle = new Pudle();
+            pudle.Bark();
+        }
+    }
+}
---
+++ C#/오버로딩/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 오버로딩
+{
+    public class Zerg 
+    {
+        public void Overload(int zerggling) {
+            Console.WriteLine("저글링 {0} 마리",zerggling);
+        }
+        public void Overload(int zerggling, int hydra) {
+            Console.WriteLine("저글링 {0} 마리 + 히드라 {1} 마리",zerggling, hydra);
+        }
+        public void Overload(int zerggling, int hydra, int lurker)
+        {
+            Console.WriteLine("저글링 {0} 마리 + 히드라 {1} 마리 + 럴커 {2}", zerggling, hydra, lurker);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zerg zerg = new Zerg();
+            zerg.Overload(1);
+            zerg.Overload(1,2);
+            zerg.Overload(1,2,3);
+
+        }
+    }
+}
