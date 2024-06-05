e505f949d1f11a435a2ed6c31e06cb21757080d8
Sooooyeon <tn7278@naver.com>
2024-06-05 17:43:07 +0900

    mongoDB 연동

--- NodeJs/Server/server.js
+++ NodeJs/Server/server.js
@@ -16,6 +16,9 @@ const express = require('express');
 const app = express(); // 서버 객체를 받음
 
 
+// body-parser 라이브러리 추가
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
 // mongoDB 연동
 let mydb;
@@ -53,6 +56,35 @@ app.get('/',function(req, res){
 
 app.get('/enter',function(req, res){
     res.sendFile(__dirname + '/enter.html');
+});
+
+app.post('/save',function(req, res){
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    // MySQL에 저장
+    // let sql = "insert into post (title, content, created) \
+    //     values (?,?, now())";
+    
+    // let params = [req.body.title, req.body.content];
+    // conn.query(sql, params, function(err, result){
+    //     if(err) throw err;
+    //     console.log('데이터 저장완료');
+    // })
+    
+
+    // 몽고DB에 데이터 저장
+    mydb.collection('post').insertOne(
+        {title : req.body.title, content : req.body.content}
+    ).then((result)=>{
+        console.log(result);
+        console.log('데이터 저장완료');
+    })
+
+
+    res.send('데이터 저장완료');
+
 });
 
 app.get('/list',function(req, res){
